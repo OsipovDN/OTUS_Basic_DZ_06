4a13da795f9b44ad146052703098fb100b031af0
OsipovDN <disorder_2@list.ru>
2022-11-20 22:42:46 +0300

    Исправление до рабочего состояния

--- src/MyContainers.cpp
+++ src/MyContainers.cpp
@@ -1,20 +1,20 @@
 #include "iostream"
 #include "myVector.h"
 
-//template <typename T>void print(const MyVector <T>& v) {
-//	for (size_t i = 0; i < v.size(); ++i) {
-//		std::cout << v[i] << " ";
-//	}
-//	std::cout << std::endl;
-//}
+template <typename T>void print(const MyVector <T>& v) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		std::cout << v[i] << " ";
+	}
+	std::cout << std::endl;
+}
 
 
 int main() {
 
-	//MyVector <int> vec1;
-	//print(vec1);
-	MyVector <char> vec2(3,5);
-	//print(vec2);
+	MyVector <int> vec1;
+	print(vec1);
+	MyVector <char> vec2(3,'x');
+	/print(vec2);
 
 
 	return 0;
