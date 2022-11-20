d35be0d591439337679ca564645bfc061b6f4daf
OsipovDN <disorder_2@list.ru>
2022-11-20 17:21:52 +0300

    Сформированы основные методы контейнера

--- MyVector/include/myVector.h
+++ MyVector/include/myVector.h
@@ -4,20 +4,47 @@
 template <typename T>
 class MyVector {
 public:
-	explicit MyVector (){}
+	MyVector ():v_ptr(nullptr),size(0){}
 
-	void push_back (T val){
-		insert(val, (size - 1));
+	explicit MyVector(size_t& count, const T& val = 0) :MyVector() {
+		size = count;
+		v_ptr = new T[size];
+		for (size_t i = 0; i < size; ++i) {
+			v_ptr[i] = val;
+		}
 	}
 
-	void insert (T val,int count){}
+	void insert(size_t& pos,int count,const T& val) {
+		if (pos < 0)
+			pos = 0;
+		if (pos > size)
+			pos = size;
+		size += count;
+		T* res_v = new T[size];
+		for (size_t i = 0; i < pos; ++i) {
+			res_v[i] = v_ptr[i];
+		}
+		for (size_t i = pos; i < (pos+count); ++i) {
+			res_v[i] = val;
+		}
+		for (size_t i = (pos + count); i < size; ++i) {
+			res_v[i] = v_ptr[i];
+		}
+		delete[]v_ptr;
+		v_ptr=res_v;
+	}
 
+	void insert(size_t& pos, const T& val) :void insert(pos, 1, val) {}
+
+	void push_back (T val){
+		insert((size - 1),1,val);
+	}
 	void erase() {
 		delete[]v_ptr;
 		v_ptr = nullptr;
 	}
 
-	T& operator [](const size_t val){}
+	T& operator [](const size_t& val){}
 
 private:
 	T* v_ptr;
---
+++ src/MyContainers.cpp
@@ -0,0 +1,10 @@
+#include "myVector.h"
+
+
+int main() {
+
+	MyVector vec;
+
+	return 0;
+}
+
